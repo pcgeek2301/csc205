dd0beb58506701c8b24c243b4d90d262f285d8be
Katy Hochstetler <katy@hochstetlers.com>
2021-02-05 18:01:44 -0700

    Starting second project mini lab 2

---
+++ Minilab_2.java
@@ -0,0 +1,11 @@
+/*
+CSC 205: <Class #37533> <Meeting days: Monday 5:00pm - 8:30pm>
+Minilab: <Minilab #2>
+Author: Katy Hochstetler
+Description: 
+*/
+public class Minilab_2 {
+    public static void main(String[] args) {
+
+    }
+}
