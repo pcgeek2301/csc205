2ae7bc164ef49589fb5cff92ea27f5431022fca4
Katy Hochstetler <katy@hochstetlers.com>
2021-02-09 11:12:34 -0700

    Testing electrical work from CSC120

--- ScratchWork.java
+++ ScratchWork.java
@@ -1,10 +1,34 @@
-import java.util.Random;
+import java.util.Scanner;
 public class ScratchWork {
     public static void main(String[] args) {
-        long seed = 5;
-        Random obj = new Random(seed);
-            
-        System.out.print(obj.nextInt(10));
+        Scanner scnr = new Scanner(System.in);
 
+        boolean endStatement = false; 
+        int A = 0;
+        int B = 0;
+        int C = 0;
+
+        System.out.println("Enter in A");
+        A = GetNumber(scnr);
+        System.out.println("Enter in B");
+        B = GetNumber(scnr);
+        System.out.println("Enter in C");
+        C = GetNumber(scnr);
+
+        if ((A != 1) || (B == 1 && C == 1)) {
+            endStatement = true;
+        }
+
+        System.out.println("The expression is " + endStatement);
+
+    }
+
+    public static int GetNumber(Scanner scnr) {
+        int number = scnr.nextInt();
+        while (number != 0 && number != 1) {
+            System.out.println("Must be 0 or 1, please reenter");
+            number = scnr.nextInt();
+        }
+        return number;
     }
 }
