966b9613223c6a3588f530191d01733baef72563
Katy Hochstetler <katy@hochstetlers.com>
2021-02-23 00:15:30 -0700

    Cleaned out scratch work for next class period

--- ScratchWork.java
+++ ScratchWork.java
@@ -1,13 +1,6 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+
 public class ScratchWork {
     public static void main(String[] args) {
-        ArrayList myList = new ArrayList();
-
-        myList.add(2);
-
-
-        System.out.println(myList);
 
     }
 }
