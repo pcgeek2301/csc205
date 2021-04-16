d576dbda8aae339bee5a6e424cca92d77c626adb
Katy Hochstetler <katy@hochstetlers.com>
2021-04-16 15:20:56 -0700

    did some testing for my electrical engineering

--- ScratchWork.java
+++
@@ -1,6 +0,0 @@
-
-public class ScratchWork {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ TesterForAssignment11.java
@@ -0,0 +1,32 @@
+
+public class TesterForAssignment11 {
+    public static void main(String[] args) {
+        boolean[][] array = {
+                            {false, false, false, false},
+                            {false, false, false, true},
+                            {false, false, true, false},
+                            {false, false, true, true},
+                            {false, true, false, false},
+                            {false, true, false, true},
+                            {false, true, true, false},
+                            {false, true, true, true},
+                            {true, false, false, false},
+                            {true, false, false, true},
+                            {true, false, true, false},
+                            {true, false, true, true},
+                            {true, true, false, false},
+                            {true, true, false, true},
+                            {true, true, true, false},
+                            {true, true, true, true}
+                        };
+
+        for(int i = 0; i < 16; i++) {
+            boolean r = f(array[i][0], array[i][1], array[i][2], array[i][3]);
+            System.out.println(r);
+        }                
+    }
+
+    public static boolean f(boolean a, boolean b, boolean c, boolean d) {
+        return ((!a) && (b) && (!c)) || ((a) && (!c) && (!d)) || ((a) && (!b) && (c)) || ((!a) && (c) && (!d));
+    }
+}
