d9a69506fcbaf0ded6f9994a1b00ec65f4a280da
Katy Hochstetler <katy@hochstetlers.com>
2021-04-19 15:49:46 -0700

    added comments and fixed two of the methods

--- Segment.java
+++ Segment.java
@@ -2,29 +2,37 @@
 CSC 205: <Class #37533> <Meeting days: Monday 5:00pm - 8:30pm>
 Minilab: <Minilab #7>
 Author: <Katy Hochstetler> <36473082>
-Description: <>
+Description: <In this segment class, I have created mutitple methods for users to create and manipulate the line segment. I have the 
+default constructor which sets the first endpoint to (1,1) and the second endpoint to (7,7). I have a 2 parameterized constructor that 
+sets two points to the endpoints. I have a method that sets one endpoint as the origin and the other as the other endpoint. I have 
+another constructor to set a deep copy. I have a method for displaying the segment information, a method to check if it equals another 
+segment, length of the segment, moving the segment, and checking if a point is an endpoint in this segment.>
 */
 
 public class Segment implements SegmentInterface {
-    protected Point leftEndpoint;
-    protected Point rightEndpoint;
+    protected Point firstEndpoint;
+    protected Point secondEndpoint;
 
+    //------------- constructors
+    // default constructor
     public Segment() {
-        this.leftEndpoint = new Point(1, 1);
-        this.rightEndpoint = new Point(7, 7);
+        this.firstEndpoint = new Point(1, 1);
+        this.secondEndpoint = new Point(7, 7);
     }
 
-    public Segment(Point leftPoint, Point rightPoint) {
-        if (leftPoint == null || rightPoint == null) {
+    // 2 parameterized constructor
+    public Segment(Point firstPoint, Point secondPoint) {
+        if (firstPoint == null || secondPoint == null) {
             throw new IllegalArgumentException("Points cannot be null");
-        } else if (leftPoint.equals(rightPoint)) {
+        } else if (firstPoint.equals(secondPoint)) {
             throw new IllegalArgumentException("Points cannot be the same, this segment would have a length of 0.");
         } else {
-            this.leftEndpoint = leftPoint;
-            this.rightEndpoint = rightPoint;
+            this.firstEndpoint = firstPoint;
+            this.secondEndpoint = secondPoint;
         }
     }
 
+    // 1 parameterized constructor 
     public Segment(Point endPoint) {
         Point originEndpoint = new Point(0, 0);
 
@@ -33,32 +41,37 @@ public class Segment implements SegmentInterface {
         } else if (endPoint.equals(originEndpoint)) {
             throw new IllegalArgumentException("Points cannot be the same, this segment would have a length of 0.");
         } else {
-            this.leftEndpoint = originEndpoint;
-            this.rightEndpoint = endPoint;
+            this.firstEndpoint = originEndpoint;
+            this.secondEndpoint = endPoint;
         }
     }
 
+    // deep copy constructor 
     public Segment(Segment userSegment) {
         if (userSegment == null) {
             throw new IllegalArgumentException("No segment received");
         } else {
-            this.leftEndpoint = userSegment.leftEndpoint;
-            this.rightEndpoint = userSegment.rightEndpoint;
+            this.firstEndpoint = userSegment.firstEndpoint;
+            this.secondEndpoint = userSegment.secondEndpoint;
         }
     }
 
+    // displays information about segment
     public String toString() {
-        return "Segment " + leftEndpoint.toString() + " " + rightEndpoint.toString();
+        return "Segment " + firstEndpoint.toString() + " " + secondEndpoint.toString();
     }
 
+    //checks if the segments are equal
     public boolean equals(Object other) {
         boolean equals;
         if (other instanceof Segment) {
             Segment s = (Segment)other;
-           if (this.leftEndpoint == s.leftEndpoint && this.rightEndpoint == s.rightEndpoint) {
-               equals = true;
+           if (this.firstEndpoint.equals(s.firstEndpoint) && this.secondEndpoint.equals(s.secondEndpoint)) {
+                equals = true;
+           } else if (this.firstEndpoint.equals(s.secondEndpoint) && this.secondEndpoint.equals(s.firstEndpoint)) {
+                equals = true;
            } else {
-               equals = false;
+                equals = false;
            }
         } else {
             equals = false;
@@ -67,23 +80,29 @@ public class Segment implements SegmentInterface {
         return equals;
     }
 
+    // displays the length of the segment 
     public double length() {
-        return this.leftEndpoint.distanceTo(this.rightEndpoint);
+        return this.firstEndpoint.distanceTo(this.secondEndpoint);
     }
 
+    // moves the segment
     public void translate(int xmove, int ymove) {
-        leftEndpoint.translate(xmove, ymove);
-        rightEndpoint.translate(xmove, ymove);
+        firstEndpoint.translate(xmove, ymove);
+        secondEndpoint.translate(xmove, ymove);
     }
 
+    // checks if a point is one of the end points in this segment
     public boolean isAnEndpoint(Point p) {
-        if (p.equals(this.leftEndpoint)) {
-            return true;
-        } else if (p.equals(this.rightEndpoint)) {
-            return true;
+        if (p == null) {
+            throw new IllegalArgumentException("No point recieved");
         } else {
-            return false;
+            if (p.equals(this.firstEndpoint)) {
+                return true;
+            } else if (p.equals(this.secondEndpoint)) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
-    
 }
